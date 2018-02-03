f2a826b26742b0750098606783a367ba366eaa89
asamwow <118westmaine@gmail.com>
2018-02-02 18:38:30 -0600

    Outlined Main Classes

---
+++ Assets/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+
+	/// Cards in the deck
+	List<Card> cards;
+
+	public Card Draw() {
+		int lastCardIndex = cards.Count - 1;
+		if (lastCardIndex < 0) {
+			return null; //TODO return a new instance of a card
+		}
+		Card card = cards[lastCardIndex];
+		cards.RemoveAt(lastCardIndex);
+		return card;
+	}
+}
---
+++ Assets/Shield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+	/// Character this shield is attached to.
+	Character character;
+
+	/// Amount of damage the shield can absorb before it is destroyed.
+	int shieldAmount;
+
+
+	/// Prefab of shield
+	static Shield shieldPrefab;
+	public static void SetShieldPrefab(Shield prefab) { Shield.shieldPrefab = prefab; }
+
+	public static Shield CreateShield(int shieldAmount, Character character) {
+		Shield newShield = Instantiate(shieldPrefab);
+		newShield.shieldAmount = shieldAmount;
+		newShield.character = character;
+		return newShield;
+	}
+}
