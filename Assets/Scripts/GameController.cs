2d33b36b8ff08d3a698038942b57632d73784c46
Austin-Scott <12504656+Austin-Scott@users.noreply.github.com>
2018-02-03 18:09:20 -0600

    Registered the cards that are properly implemented with the GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -26,7 +26,42 @@ public class GameController : MonoBehaviour {
 	void Start() {
 		currentBoard = Instantiate(boardPrefab);
 		StartCoroutine(currentBoard.Initialize(Instantiate(characterPrefab), Instantiate(characterPrefab)));
-	}
+
+        //Load all cards into game
+        CreateCard(typeof(CardSlash));
+        CreateCard(typeof(CardPsychUp));
+        CreateCard(typeof(CardStab));
+        CreateCard(typeof(CardPhysicallyFit));
+        CreateCard(typeof(CardHalberdStrike));
+        CreateCard(typeof(CardPunch));
+        CreateCard(typeof(CardSteroids));
+        CreateCard(typeof(CardTackle));
+        CreateCard(typeof(CardPistolShot));
+        CreateCard(typeof(CardBuckshot));
+        CreateCard(typeof(CardLightningBolt));
+        CreateCard(typeof(CardRifleShot));
+        CreateCard(typeof(CardReload));
+        CreateCard(typeof(CardShowOff));
+        CreateCard(typeof(CardQuickFingers));
+        CreateCard(typeof(CardThrownDagger));
+        CreateCard(typeof(CardOilWeapon));
+        CreateCard(typeof(CardAim));
+        CreateCard(typeof(CardRest));
+        CreateCard(typeof(CardTowerShield));
+        CreateCard(typeof(CardBandage));
+        CreateCard(typeof(CardDoctorsBag));
+        CreateCard(typeof(CardPlan));
+        CreateCard(typeof(CardPrayer));
+        CreateCard(typeof(CardSeduce));
+        CreateCard(typeof(CardIntellectualism));
+        CreateCard(typeof(CardIntimidate));
+        CreateCard(typeof(CardTerrify));
+        CreateCard(typeof(CardSouless));
+        CreateCard(typeof(CardFranticThinking));
+        //TODO: Add the remaining cards when they are properly implemented
+
+
+    }
 
 	/// Creates a card of type
 	public static Card CreateCard(System.Type cardType) {
